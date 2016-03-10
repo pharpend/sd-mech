8d0b7af570174a7743f4c8fbb6f1bd9bd4fed0f7
Peter Harpending <peter@harpending.org>
2016-03-09 17:12:09 -0700

    MarkovSpec: write some functions
    I wrote three functions
    
    * 'generateIteration', singular, generates a bunch of 'Event's.
    * 'generateIterations n', plural, runs 'generateIteration' n times.
    * 'runEvent', takes an event, and translates it into an action in the
      'EMechM' monad.

--- spec/MarkovSpec.hs
+++ spec/MarkovSpec.hs
@@ -6,7 +6,66 @@ import MarkovTypes
 import SdMech
 
 import Test.Hspec
+import Test.QuickCheck
 
 spec :: Spec
 spec = context "Randomly-generated simulation" $ do
     return ()
+
+-- |Generate a number of iterations. 'RunIteration' is not manually
+-- interspersed.
+generateIterations :: Int -> Gen [[Event]]
+generateIterations n =
+    vectorOf n generateIteration
+
+-- |Generate a number of arbitrary 'Event's to run (critically, not
+-- 'RunIteration', though).
+generateIteration :: Gen [Event]
+generateIteration =
+    listOf $ arbitrary `suchThat` (/= RunIteration)
+
+-- |Run an event
+runEvent :: Event -> EMechM ()
+runEvent = \case
+    RunIteration -> do
+        return ()
+
+    PatrSpawn patr -> do
+        return ()
+
+    PatrDie patr -> do
+        return ()
+
+    PatrDeposit patr funds' -> do
+        return ()
+
+    PatrWithdraw patr funds' -> do
+        return ()
+
+    PatrMkPledge patr prj -> do
+        return ()
+
+    PatrRescindPledge patr prj -> do
+        return ()
+
+    PatrSuspendPledge patr prj -> do
+        return ()
+
+    PrjSpawn prj -> do
+        return ()
+
+    PrjDie prj -> do
+        return ()
+
+    PrjDeposit prj funds' -> do
+        return ()
+
+    PrjWithdraw prj funds' -> do
+        return ()
+
+    PrjRescindPledge prj patr -> do
+        return ()
+
+    PrjSuspendPledge prj patr -> do
+        return ()
+
